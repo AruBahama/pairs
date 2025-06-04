6f5ab52e8127daebda836f7b1cb30c7212260f83
AruBahama <arubahama13@gmail.com>
2025-06-04 15:44:57 -0500

    Update feature_engineer.py
    added yfinance as mandatory

--- src/data/feature_engineer.py
+++ src/data/feature_engineer.py
@@ -8,7 +8,12 @@ def engineer(ticker:str):
     df = pd.read_csv(RAW_DIR/f"{ticker}.csv",index_col=0,parse_dates=True)
     df_ta = ta.strategy(ta.Strategy(name='all',talib=False),df.copy())
     df = pd.concat([df, df_ta], axis=1).dropna()
-    tk = Toolkit(ticker, start=df.index.min(), end=df.index.max())
+        tk = Toolkit(
+        ticker,
+        start=df.index.min(),
+        end=df.index.max(),
+        enforce_source="YahooFinance",
+    )
     funda = tk.ratios.collect().ffill().reindex(df.index)
     df = pd.concat([df, funda], axis=1).dropna()
     out = PROC_DIR/f"{ticker}.parquet"
