8ce73735ef5dd0940bc6cc70949ec56bcf8d191a
AruBahama <arubahama13@gmail.com>
2025-06-04 22:39:17 -0500

    aggregate latents per ticker (#16)
    this is changing the clustering from cae clustering (mistake) to stock clustering

--- src/clustering/select_pairs.py
+++ src/clustering/select_pairs.py
@@ -1,18 +1,24 @@
 
+import json
 import numpy as np
 from scipy.spatial.distance import cdist
 from ..config import PAIRS_PER_CLUST, LOG_DIR
 
 def select_pairs():
-    Z = np.load(LOG_DIR/'latent.npy')
-    labels = np.load(LOG_DIR/'labels.npy')
-    pairs=[]
+    Z = np.load(LOG_DIR / 'ticker_latent.npy')
+    labels = np.load(LOG_DIR / 'labels.npy')
+    with open(LOG_DIR / 'ticker_index.json') as f:
+        tickers = json.load(f)
+
+    pairs = []
     for c in set(labels):
         idx = np.where(labels==c)[0]
         dists = cdist(Z[idx], Z[idx], metric='euclidean')
         triu = np.triu_indices_from(dists, k=1)
         sorted_pairs = sorted(zip(triu[0],triu[1],dists[triu]), key=lambda x:x[2])
         top = sorted_pairs[:PAIRS_PER_CLUST]
-        pairs.extend([(idx[i], idx[j]) for i,j,_ in top])
-    np.save(LOG_DIR/'pairs.npy', pairs)
+        pairs.extend([(tickers[idx[i]], tickers[idx[j]]) for i, j, _ in top])
+
+    # Save as numpy array of strings for later steps
+    np.save(LOG_DIR / 'pairs.npy', np.array(pairs, dtype=object))
     return pairs
