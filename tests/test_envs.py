0ade851dd608306e992ab6de4d459817136c3b7e
AruBahama <arubahama13@gmail.com>
2025-06-06 22:58:38 -0500

    Validate actions in PairTradingEnv (#59)

--- tests/test_envs.py
+++ tests/test_envs.py
@@ -9,11 +9,20 @@ from src.rl.envs import PairTradingEnv
 
 
 def test_env_basic_step():
-    price1 = pd.Series(np.arange(10, dtype=float))
-    price2 = pd.Series(np.arange(10, dtype=float))
+    price1 = pd.Series(np.arange(100, dtype=float))
+    price2 = pd.Series(np.arange(100, dtype=float))
     env = PairTradingEnv(price1, price2)
     obs, _ = env.reset()
     assert obs.shape[0] == env.observation_space.shape[0]
     obs, reward, done, trunc, info = env.step(1)
     assert isinstance(reward, float)
     assert obs.shape == env.observation_space.shape
+
+
+def test_env_invalid_action():
+    price1 = pd.Series(np.arange(100, dtype=float))
+    price2 = pd.Series(np.arange(100, dtype=float))
+    env = PairTradingEnv(price1, price2)
+    env.reset()
+    with pytest.raises(ValueError):
+        env.step(42)
